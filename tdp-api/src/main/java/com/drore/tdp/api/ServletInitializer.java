c4e08fc7315eaa4eb932aa1f37ec439476f535d7
zengl <zengl@drore.com>
2019-01-24 10:32:05 +0800

    接口对接模块环境搭建

---
+++ tdp-api/src/main/java/com/drore/tdp/api/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.drore.tdp.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TdpApiApplication.class);
+	}
+
+}
+
---
+++ tdp-api/src/main/java/com/drore/tdp/api/TdpApiApplication.java
@@ -0,0 +1,14 @@
+package com.drore.tdp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdpApiApplication.class, args);
+	}
+
+}
+
